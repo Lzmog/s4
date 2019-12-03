9963ba6e1fe0fd539b867dbe445da519b9d56cd0
Lukas Zmoginas <lukas.zmoginas@nfq.lt>
2019-12-03 19:49:52 +0200

    Video 17 | When Migrations Fail  | Doctrine

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -56,6 +56,16 @@ class Article
      */
     private $imageFileName;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updateAt;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -179,6 +189,30 @@ class Article
     public function incrementHeartCount(): self
     {
         $this->heartCount = $this->heartCount + 1;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(\DateTimeInterface $updateAt): self
+    {
+        $this->updateAt = $updateAt;
 
         return $this;
     }
